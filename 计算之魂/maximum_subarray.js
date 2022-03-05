70698b3d9bf2c36d9b46c0a226b141d228a9dde2
zhangjinglearning <zhangjinglearning@gmail.com>
2022-03-05 11:44:17 +0800

    这不是分治吧，不过算是两头并行，哈哈哈哈

--- 计算之魂/maximum_subarray.js
+++ 计算之魂/maximum_subarray.js
@@ -18,3 +18,45 @@ nums.forEach((curVal, index) => {
 });
 
 console.log(`sum[${startIndex}, ${endIndex}] = ${maxSum}`);
+
+// solution two: forward and backward
+// const numList = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+const numList = [1, 1, 1, 1, -5, 1, 1, 1, 1];
+let forwardMaxIndex = 0;
+let backwardMaxIndex = numList.length - 1;
+let forwardMaxSum = 0;
+let backwardMaxSum = 0;
+
+let forwardCurSum = 0;
+numList.forEach((curVal, index) => {
+  forwardCurSum += curVal;
+  if (forwardCurSum > forwardMaxSum) {
+    forwardMaxSum = forwardCurSum;
+    forwardMaxIndex = index;
+  }
+});
+console.log(`forwardMaxIndex: ${forwardMaxIndex}, forwardMaxSum: ${forwardMaxSum}`);
+
+let backwardCurSum = 0;
+for (let i = numList.length - 1; i >= 0; i--) {
+  backwardCurSum += numList[i];
+  if (backwardCurSum > backwardMaxSum) {
+    backwardMaxSum = backwardCurSum;
+    backwardMaxIndex = i;
+  }
+}
+console.log(`backwardMaxIndex: ${backwardMaxIndex}, backwardMaxSum: ${backwardMaxSum}`);
+
+if (forwardMaxIndex > backwardMaxIndex) {
+  let sum = 0;
+  for (let i = backwardMaxIndex; i <= forwardMaxIndex; i++) {
+    sum += numList[i];
+  }
+  console.log(`sum[${backwardMaxIndex}, ${forwardMaxIndex}] = ${sum}`);
+} else {
+  if (forwardMaxSum > backwardMaxSum) {
+    console.log(`sum[0, ${forwardMaxIndex}] = ${forwardMaxSum}`);
+  } else {
+    console.log(`sum[${backwardMaxIndex}, ${numList.length-1}] = ${backwardMaxSum}`);
+  }
+}
